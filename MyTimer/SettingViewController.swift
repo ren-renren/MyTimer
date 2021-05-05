f007064cf9daa8e8d1f4684a9456f0155d86c105
ren-renren <ren.alpineski.1003@gmail.com>
2021-05-05 23:46:27 +0900

    210505_23:46版

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingViewController.swift
+//  MyTimer
+//
+//  Created by 鈴木蓮 on 2021/05/05.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+	@IBOutlet weak var timerSettingPicker: UIPickerView!
+	
+	@IBAction func decisionButtonAction(_ sender: Any) {
+	}
+	
+}
